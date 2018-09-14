c6b57c4150027a4414375426b6619d3be6b8229b
Hauvgaard <you@example.com>
2018-09-14 14:42:21 +0200

    Tror det virker nu at bruge option classen

--- src/program1/Options.java
+++ src/program1/Options.java
@@ -7,17 +7,16 @@ import java.util.GregorianCalendar;
 
 
 public class Options {
-     public void setInt(Int option) { {
-    Calendar c = GregorianCalendar.getInstance();
+     public static void setInt() {
             java.util.Scanner ind = new java.util.Scanner(System.in);
             int option;
             System.out.println("Hvad vil du se? (Tast nummer)");
             System.out.println("1. Se dato");
             System.out.println("2. Se tid");
             System.out.println("3. Info om forfatteren");
+            
             option = ind.nextInt();
-           
-          this.option = option;
+            Program1.option = option;
             
      }
      
--- src/program1/Program1.java
+++ src/program1/Program1.java
@@ -5,18 +5,19 @@ import java.util.GregorianCalendar;
 
 
 public class Program1 {
-    
+
+    public int option;
+
     public static void main(String[] args) {
-        OUTER:
         while (true) {
             Calendar c = GregorianCalendar.getInstance();
-            java.util.Scanner ind = new java.util.Scanner(System.in);
-            int option;
+            /*java.util.Scanner ind = new java.util.Scanner(System.in);
             System.out.println("Hvad vil du se? (Tast nummer)");
             System.out.println("1. Se dato");
             System.out.println("2. Se tid");
             System.out.println("3. Info om forfatteren");
-            option = ind.nextInt();
+            option = ind.nextInt();*/
+            option = Options.setInt();
             switch (option) {
                 case 1:
                     System.out.println("Datoen er " + c.get(Calendar.DAY_OF_MONTH) + "/" + c.get(Calendar.MONTH) + "/" + c.get(Calendar.YEAR));
@@ -28,16 +29,17 @@ public class Program1 {
                     System.out.println("Forfatteren hedder Alexander og dette er en af hans første programmer.");
                     break;
                 case 99:
-                    break OUTER;
+                    return;
                 default:
                     break;
             } 
+            
             try{
                 Thread.sleep(5000);
             }
             catch(InterruptedException ex){
                 Thread.currentThread().interrupt();
-            }}
+            }
     }
     
 }
